b6a91124b8a40730b7553cc70af9edb7288934c3
ninjascare <you@example.com>
2019-04-18 15:32:28 -0400

    had to fix a typo

--- app/screens/feed.js
+++ app/screens/feed.js
@@ -27,11 +27,18 @@ export default class feed extends Component {
             <Text>@kevin</Text>
           </View>
           <View>
-            <Image source={{ uri: "https://source.unsplash.com/random/500x"+ Math.floor((Math.random() * 800) + 500) }} style={{resizeMode: 'cover', width:'100%', height: 275}}/>
+            <Image
+              source={{
+                uri:
+                  "https://source.unsplash.com/random/500x" +
+                  Math.floor(Math.random() * 800 + 500)
+              }}
+              style={{ resizeMode: "cover", width: "100%", height: 275 }}
+            />
           </View>
           <View>
-               <Text>Caption text here...</Text>
-               <Text>View Comments...</Text>
+            <Text>Caption text here...</Text>
+            <Text>View Comments...</Text>
           </View>
         </View>
       </View>
