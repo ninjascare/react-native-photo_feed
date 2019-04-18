815317cb67d052ab2b3e4955b82793793af0e316
ninjascare <you@example.com>
2019-04-18 15:31:21 -0400

    got data on my page from unsplash images

--- app/screens/feed.js
+++ app/screens/feed.js
@@ -7,8 +7,33 @@ export default class feed extends Component {
   }
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Feed Page</Text>
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            height: 70,
+            paddingTop: 30,
+            backgroundColor: "white",
+            borderColor: "lightgrey",
+            borderBottomWidth: 0.5,
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <Text>Feed Page</Text>
+        </View>
+        <View>
+          <View>
+            <Text>Time Ago</Text>
+            <Text>@kevin</Text>
+          </View>
+          <View>
+            <Image source={{ uri: "https://source.unsplash.com/random/500x"+ Math.floor((Math.random() * 800) + 500) }} style={{resizeMode: 'cover', width:'100%', height: 275}}/>
+          </View>
+          <View>
+               <Text>Caption text here...</Text>
+               <Text>View Comments...</Text>
+          </View>
+        </View>
       </View>
     );
   }
