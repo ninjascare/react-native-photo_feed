6fa31ebb8b437d85d4dc2f6d36d468d4acff5e86
ninjascare <kevin76475@gmail.com>
2019-04-22 17:28:46 -0400

    added more interface to my upload page

--- app/screens/upload.js
+++ app/screens/upload.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Flatlist, StyleSheet, Text, View, Image } from "react-native";
+import {
+  TouchableOpacity,
+  Flatlist,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from "react-native";
 import { f, auth, database, storage } from "../../Config/config";
 
 export default class upload extends Component {
@@ -9,6 +16,10 @@ export default class upload extends Component {
       loggedIn: false
     };
   }
+
+  findNewImage = () => {
+    //
+  };
 
   componentDidMount = () => {
     var that = this;
@@ -29,12 +40,29 @@ export default class upload extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ flex: 1 }}>
         {this.state.loggedIn == true ? (
           // are loggeed in
-          <Text>Upload Page</Text>
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <Text style={{ fontSize: 28, paddingBottom: 15 }}>Upload Page</Text>
+            <TouchableOpacity
+              onPress={() => this.findNewImage()}
+              style={{
+                paddingVertical: 10,
+                paddingHorizontal: 20,
+                backgroundColor: "blue",
+                borderRadius: 5
+              }}
+            >
+              <Text style={{ color: "white" }}>Select Photo</Text>
+            </TouchableOpacity>
+          </View>
         ) : (
-          <View>
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
             <Text>Please login to upload new content</Text>
           </View>
         )}
