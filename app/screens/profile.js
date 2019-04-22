ace81170a2ba2a44debff506b72c6d6f3585412c
ninjascare <kevin76475@gmail.com>
2019-04-22 13:07:56 -0400

    created the profile page

--- app/screens/profile.js
+++ app/screens/profile.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Flatlist, StyleSheet, Text, View, Image } from "react-native";
+import {
+  TouchableOpacity,
+  Flatlist,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from "react-native";
 import { f, auth, database, storage } from "../../Config/config";
 
 export default class profile extends Component {
@@ -29,12 +36,53 @@ export default class profile extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ flex: 1 }}>
         {this.state.loggedIn == true ? (
           // are loggeed in
-          <Text>Profile Page</Text>
+          <View style={{ flex: 1 }}>
+            <View
+              style={{
+                height: 70,
+                paddingTop: 30,
+                backgroundColor: "white",
+                borderColor: "lightgrey",
+                borderBottomWidth: 0.5,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Text>Profile</Text>
+            </View>
+            <View
+              style={{
+                justifyContent: "space-evenly",
+                alignItems: "center",
+                flexDirection: "row",
+                paddingVertical: 10
+              }}
+            >
+              <Image
+                source={{
+                  uri: "https://api.adorable.io/avatars/285/test@user.i.png"
+                }}
+                style={{
+                  marginLeft: 10,
+                  width: 100,
+                  height: 100,
+                  borderRadius: 50
+                }}
+              />
+
+              <View style={{ marginRight: 10 }}>
+                <Text>name</Text>
+                <Text>@username</Text>
+              </View>
+            </View>
+          </View>
         ) : (
-          <View>
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
             <Text>Please login to view your profile </Text>
           </View>
         )}
