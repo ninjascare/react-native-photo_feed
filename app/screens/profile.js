451f71141ded48d63e9f4419add67ffbdb299c42
ninjascare <kevin76475@gmail.com>
2019-04-29 16:07:19 -0400

    got the edit profile button working now!!

--- app/screens/profile.js
+++ app/screens/profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {
+  TextInput,
   TouchableOpacity,
   Flatlist,
   StyleSheet,
@@ -18,21 +19,71 @@ export default class profile extends Component {
     };
   }
 
+  fetchUserInfo = userId => {
+    let that = this;
+    database
+      .ref("users")
+      .child(userId)
+      .once("value")
+      .then(snapshot => {
+        const exists = snapshot.val() != null;
+        if (exists) data = snapshot.val();
+        that.setState({
+          username: data.username,
+          name: data.name,
+          avatar: data.avatar,
+          loggedIn: true,
+          userId: userId
+        });
+      });
+  };
+
   componentDidMount = () => {
     var that = this;
     f.auth().onAuthStateChanged(user => {
       if (user) {
         // logged in
-        that.setState({
-          loggedIn: true,
-          userId: user.uid
-        });
+        that.fetchUserInfo(user.uid);
       } else {
         // not logged in
         that.setState({
           loggedIn: false
         });
       }
+    });
+  };
+
+  logOutUser = () => {
+    f.auth().signOut();
+    alert("LoggedOut");
+  };
+
+  editProfile = () => {
+    this.setState({
+      editingProfile: true
+    });
+  };
+
+  saveProfile = () => {
+    let name = this.state.name;
+    let username = this.state.username;
+
+    if (name != "") {
+      database
+        .ref("users")
+        .child(this.state.userId)
+        .child("name")
+        .set(name);
+    }
+    if (username != "") {
+      database
+        .ref("users")
+        .child(this.state.userId)
+        .child("username")
+        .set(username);
+    }
+    this.setState({
+      editingProfile: false
     });
   };
 
@@ -65,7 +116,7 @@ export default class profile extends Component {
             >
               <Image
                 source={{
-                  uri: "https://api.adorable.io/avatars/285/test@user.i.png"
+                  uri: this.state.avatar
                 }}
                 style={{
                   marginLeft: 10,
@@ -76,58 +127,130 @@ export default class profile extends Component {
               />
 
               <View style={{ marginRight: 10 }}>
-                <Text>name</Text>
-                <Text>@username</Text>
+                <Text>{this.state.name}</Text>
+                <Text>{this.state.username}</Text>
               </View>
             </View>
-            <View style={{ paddingBottom: 20, borderBottomWidth: 1 }}>
-              <TouchableOpacity
+            {this.state.editingProfile == true ? (
+              <View
                 style={{
-                  marginTop: 10,
-                  marginHorizontal: 40,
-                  paddingVertical: 15,
-                  borderRadius: 20,
-                  borderColor: "gray",
-                  borderWidth: 1.5
+                  alignItems: "center",
+                  justifyContent: "center",
+                  paddingBottom: 20,
+                  borderBottomWidth: 1
                 }}
               >
-                <Text style={{ textAlign: "center", color: "gray" }}>
-                  Logout
-                </Text>
-              </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() =>
+                    this.setState({
+                      editingProfile: false
+                    })
+                  }
+                >
+                  <Text style={{ fontWeight: "bold", color: "red" }}>
+                    {" "}
+                    Cancel editing
+                  </Text>
+                </TouchableOpacity>
+                <Text>Name:</Text>
+                <TextInput
+                  editable={true}
+                  placeholder="Enter your name.."
+                  onChangeText={text =>
+                    this.setState({
+                      name: text
+                    })
+                  }
+                  value={this.state.name}
+                  style={{
+                    width: 250,
+                    marginVertical: 10,
+                    padding: 5,
+                    borderColor: "gray",
+                    borderWidth: 1
+                  }}
+                />
+                <Text>Username:</Text>
+                <TextInput
+                  editable={true}
+                  placeholder="Enter your username.."
+                  onChangeText={text =>
+                    this.setState({
+                      username: text
+                    })
+                  }
+                  value={this.state.username}
+                  style={{
+                    width: 250,
+                    marginVertical: 10,
+                    padding: 5,
+                    borderColor: "gray",
+                    borderWidth: 1
+                  }}
+                />
+                <TouchableOpacity
+                  style={{ backgroundColor: "blue", padding: 10 }}
+                  onPress={() => this.saveProfile()}
+                >
+                  <Text style={{ fontWeight: "bold", color: "white" }}>
+                    Save Changes
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            ) : (
+              <View style={{ paddingBottom: 20, borderBottomWidth: 1 }}>
+                <TouchableOpacity
+                  style={{
+                    marginTop: 10,
+                    marginHorizontal: 40,
+                    paddingVertical: 15,
+                    borderRadius: 20,
+                    borderColor: "gray",
+                    borderWidth: 1.5
+                  }}
+                >
+                  <Text
+                    onPress={() => this.logOutUser()}
+                    style={{ textAlign: "center", color: "gray" }}
+                  >
+                    Logout
+                  </Text>
+                </TouchableOpacity>
 
-              <TouchableOpacity
-                style={{
-                  marginTop: 10,
-                  marginHorizontal: 40,
-                  paddingVertical: 15,
-                  borderRadius: 20,
-                  borderColor: "gray",
-                  borderWidth: 1.5
-                }}
-              >
-                <Text style={{ textAlign: "center", color: "gray" }}>
-                  Edit Profile
-                </Text>
-              </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => this.editProfile()}
+                  style={{
+                    marginTop: 10,
+                    marginHorizontal: 40,
+                    paddingVertical: 15,
+                    borderRadius: 20,
+                    borderColor: "gray",
+                    borderWidth: 1.5
+                  }}
+                >
+                  <Text style={{ textAlign: "center", color: "gray" }}>
+                    Edit Profile
+                  </Text>
+                </TouchableOpacity>
 
-              <TouchableOpacity
-                onPress={() => this.props.navigation.navigate("Upload")}
-                style={{
-                  backgroundColor: "gray",
-                  marginTop: 10,
-                  marginHorizontal: 40,
-                  paddingVertical: 15,
-                  borderRadius: 20,
-                  borderColor: "gray",
-                  borderWidth: 1.5
-                }}
-              >
-                <Text style={{ textAlign: "center", color: "white" }}>
-                  Upload New +
-                </Text>
-              </TouchableOpacity>
-            </View>
+                <TouchableOpacity
+                  onPress={() => this.props.navigation.navigate("Upload")}
+                  style={{
+                    backgroundColor: "gray",
+                    marginTop: 10,
+                    marginHorizontal: 40,
+                    paddingVertical: 15,
+                    borderRadius: 20,
+                    borderColor: "gray",
+                    borderWidth: 1.5
+                  }}
+                >
+                  <Text style={{ textAlign: "center", color: "white" }}>
+                    Upload New +
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            )}
             <PhotoList
               isUser={true}
               userId={this.state.userId}
